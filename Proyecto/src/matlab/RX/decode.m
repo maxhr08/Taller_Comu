50750398d602da1f5baa774aee7f5276251e811a
Max <maxhr08@gmail.com>
2019-11-24 14:35:19 -0600

    'changed_decoder'

---
+++ Proyecto/src/matlab/RX/decode.m
@@ -0,0 +1,19 @@
+function [correctedcode]= decode(Encoded_Message)
+% Use a [15,11] Hamming code.
+    m = 4; 
+   
+    newH=hammgen(m);
+    
+    trt = syndtable(newH); % Produce decoding table.
+    recd = Encoded_Message; % Suppose this is the received vector.
+    
+    syndrome = rem(recd * newH',2);
+    syndrome_de = bi2de(syndrome,'left-msb'); % Convert to decimal.
+    
+    disp(['Syndrome = ',num2str(syndrome_de),...
+          ' (decimal), ',num2str(syndrome),' (binary)'])
+    corrvect = trt(1+syndrome_de,:); % Correction vector
+    
+    % Now compute the corrected codeword.
+    correctedcode = rem(corrvect+recd,2);
+end
