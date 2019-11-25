f2c874b8d552759a27605ae55d10435e639cbd24
MartinOO16 <41214795+MartinOO16@users.noreply.github.com>
2019-11-25 07:12:36 -0600

    modified_python_src

--- Proyecto/src/python/TallerToSerial.py
+++ Proyecto/src/python/TallerToSerial.py
@@ -2,31 +2,45 @@ import serial
 import time
 
 transmissions=1
-EncodedMessage=open("txt/enmessage.txt","r") #Se abre el mensaje codificado
-en=EncodedMessage.read()
-FirstByte=int(("0b"+en[8:15]),2) 
-FirstByteChar=FirstByte.to_bytes((FirstByte.bit_length() + 7) // 8, 'big').decode()#Se codifica para enviarse como un caracter al arduino
-SecondByte=int(("0b0"+en[0:7]),2)
-SecondByteChar=SecondByte.to_bytes((SecondByte.bit_length() + 7) // 8, 'big').decode() #Se envian dos bytes porque el arduino lee desde el buffer por byte
 
-print(FirstByteChar)
-print(SecondByteChar)
+def Ordenar(en):    
+    FirstByte=int(("0b"+en[8:15]),2) 
+    FirstByteChar=FirstByte.to_bytes((FirstByte.bit_length() + 7) // 8, 'big').decode()#Se codifica para enviarse como un caracter al arduino
+    SecondByte=int(("0b0"+en[0:7]),2)
+    SecondByteChar=SecondByte.to_bytes((SecondByte.bit_length() + 7) // 8, 'big').decode() #Se envian dos bytes porque el arduino lee desde el buffer por byte
+    print(FirstByteChar)
+    print(SecondByteChar)
+    return FirstByteChar,SecondByteChar
 
 def main():
     
     print("hi")
-    ser1=serial.Serial('COM3',9600) #Se abre el puerto de comunicacion
-    time.sleep(1)
-    for k in range(transmissions):       
-        ser1.write(FirstByteChar.encode())
-        time.sleep(1)
-        ser1.write(SecondByteChar.encode())
+    ser1=serial.Serial('COM8',9600) #Se abre el puerto de comunicacion
+    time.sleep(2)
+    EncodedMessage=open("../Matlab/TX/mensaje_codificado.txt","r") #Se abre el mensaje codificado
+    en=EncodedMessage.read().splitlines()
+    print(en)
+
+    
+    for i in range(len(en)):
+
+        #binary=format(int(en[i],2),'018b')
+        time.sleep(0.5)
+        #[FirstByteChar, SecondByteChar]=Ordenar(en[k])
+        #FirstBytechar=en[0:7]
+        #SecondByteChar=en[8:15]
+        #time.sleep(0.4)
+        #print(binary)
+        ser1.write(en[i])
+        
+        #time.sleep(0.6)
+        #ser1.write(FirstByteChar.encode())
+        #time.sleep(0.5)
         print("sent")
+
+    parada=b'\r'
+    ser1.write(parada)    
     ser1.close()
-
+    EncodedMessage.close()
+    
 main()
-EncodedMessage.close()
-
-
-
-
